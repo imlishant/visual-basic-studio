a0245b6ccad08390ac93f176d370d1d1e6bff51a
imlishant <lishantsahu@gmail.com>
2017-10-14 17:44:49 +0530

    Stopwatch millisec
    Bug: ms goes upto 60

---
+++ Stopwatch.vb
@@ -0,0 +1,46 @@
+﻿Public Class Stopwatch
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Label1.Text = Val(Label1.Text) + 1
+        If Val(Label1.Text) > 59 Then
+            Label2.Text = Val(Label2.Text) + 1
+            Label1.Text = Val(Label1.Text) Mod 60
+
+        End If
+        If Val(Label2.Text) > 59 Then
+            Label3.Text = Val(Label3.Text) + 1
+            Label2.Text = Val(Label2.Text) Mod 60
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Label1.Text = "0"
+        Label2.Text = "0"
+        Label3.Text = "0"
+    End Sub
+End Class
